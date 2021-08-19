41411098c7c91c8b6836b67806de473881c42bab
Kiet Pham <kietpham.dev@gmail.com>
2021-08-19 16:52:18 +1000

    Customer Order: Test display all orders

--- AspNetCorePostgreSQLDockerApp/Client/src/app/customers/components/customer-orders/customer-orders.component.spec.ts
+++ AspNetCorePostgreSQLDockerApp/Client/src/app/customers/components/customer-orders/customer-orders.component.spec.ts
@@ -1,45 +1,23 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
 import {CustomerOrdersComponent} from './customer-orders.component';
 import {CustomersModule} from "../../customers.module";
 import {ActivatedRoute, Router} from "@angular/router";
 import {of} from "rxjs";
 import {RouterTestingModule} from "@angular/router/testing";
+import {CUSTOMERS} from "../../../../common/customer-data";
 
 describe('CustomerOrdersComponent', () => {
   let component: CustomerOrdersComponent;
   let fixture: ComponentFixture<CustomerOrdersComponent>;
+  const customer = CUSTOMERS[0];
+  const customerOrders = customer.orders;
 
   const activatedRouteMock = {
     snapshot: {
       data: {
-        customer: {
-          "id": 1,
-          "firstName": "Pinal",
-          "lastName": "Dave",
-          "email": "Pinal.Dave@gmail.com",
-          "fullName": "Pinal Dave"
-        },
-        orders: [
-          {
-            "id": 96,
-            "product": "Needes",
-            "quantity": 1,
-            "price": 5.99,
-            "customerId": 1,
-            "status": "InProgress",
-            "orderTotal": 5.99
-          },
-          {
-            "id": 97,
-            "product": "Speakers",
-            "quantity": 1,
-            "price": 499.99,
-            "customerId": 1,
-            "orderTotal": 499.99,
-            "status": "InProgress"
-          },
-        ],
+        customer,
+        orders: customerOrders,
       },
     },
     queryParams: of({id: 1})
@@ -63,7 +41,20 @@ describe('CustomerOrdersComponent', () => {
   }));
 
   it('should create the component', () => {
+
     expect(component).toBeTruthy();
+
   })
+
+  it('should display all orders', fakeAsync(() => {
+
+    const customerOrdersComponent: any = component;
+    console.log(customerOrdersComponent.route.snapshot.data);
+    const orders = customerOrdersComponent.route.snapshot.data.orders;
+    expect(orders).toEqual(customerOrders);
+
+  }));
+
+  
 
 });
