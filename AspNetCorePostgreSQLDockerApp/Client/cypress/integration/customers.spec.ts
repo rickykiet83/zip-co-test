e7a9ea24190239e01c07d7ea19a7d5213516b2b7
Kiet Pham <kietpham.dev@gmail.com>
2021-08-22 17:52:46 +1000

    test view orders e2e

--- AspNetCorePostgreSQLDockerApp/Client/cypress/integration/customers.spec.ts
+++ AspNetCorePostgreSQLDockerApp/Client/cypress/integration/customers.spec.ts
@@ -17,5 +17,24 @@ describe('Customer List Page', () => {
 
     cy.get('.row.customer-list').should('have.lengthOf.at.least', 29);
 
+  });
+
+  it('should display popup add new customer form after click', () => {
+    cy.get('#add-customer-link').click();
+
+    cy.get('.jw-modal').should('be.ok');
+  });
+
+  it('should close popup add new customer form after cancel button is clicked', () => {
+    cy.get('#add-customer-link').click();
+    cy.get('#btn-close-modal').click();
+  });
+
+  it('should display order of customer was selected', () => {
+    cy.get('.btn-view-orders').first().click();
+
+    cy.get('#revenue').should('be.visible');
   })
+
+
 })
